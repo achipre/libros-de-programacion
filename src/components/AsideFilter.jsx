fcdd6a253e66f4f51371fcf17f23d5232008f950
Alex Chipre <alechipre@gmail.com>
2023-12-15 20:14:47 -0500

    Add Info Aside

--- src/components/AsideFilter.jsx
+++ src/components/AsideFilter.jsx
@@ -1,37 +1,44 @@
-import './asideFilter.css'
-import Books from '../mockup/books.json'
 import { useState } from 'react'
+import Books from '../mockup/books.json'
+import './asideFilter.css'
+
 export default function AsideFilter () {
-  const tecnologies = Books.map(tech => tech.Lenguaje).sort()
+  const lenguajesObj = Books.filter(tech => tech.Categoria === 'Lenguaje de Programacion' && { ...tech })
+  const lenguajesArr = Array.from(new Set(lenguajesObj.map(tech => tech.Lenguaje))).filter(no => no !== 'General').sort()
+  const lengujesOrden = lenguajesArr.toSpliced(0, 0, 'General')
+
   const [programCheck, setProgramCheck] = useState(false)
   const handlechekProgra = () => {
     setProgramCheck(!programCheck)
   }
-  console.log(tecnologies)
   return (
     <aside className="aside-filter">
       <section onClick={handlechekProgra} className="section-filter">
         <h2>Programación </h2>
-        <input type="checkbox" checked={programCheck} hidden />
-        <span>⌃</span>
+        <input type="checkbox" checked={programCheck} readOnly hidden />
+        <span className={`arrow ${programCheck && 'arrowback'}`}>⌃</span>
       </section>
-      <div className="section-program">
-        {tecnologies.map((tech, i) => (
-          <p className="tecnology" key={i}>
-            {tech}
+      <div className={`section-program  ${!programCheck && 'hidden'}`}>
+        {lengujesOrden.map((leng, idx) => (
+          <p className="tecnology" key={idx}>
+            {leng}
           </p>
         ))}
       </div>
       <hr />
       <section className="section-filter">
         <h2>Diseño UI/UX</h2>
-        <span>⌃</span>
+        <span className="arrow">⌃</span>
       </section>
-
       <hr />
       <section className="section-filter">
         <h2>Base de Datos</h2>
-        <span>⌃</span>
+        <span className="arrow">⌃</span>
+      </section>
+      <hr />
+      <section className="section-filter">
+        <h2>Otros</h2>
+        <span className="arrow">⌃</span>
       </section>
     </aside>
   )
