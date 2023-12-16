66cd3feba85e67ab3ece631d47e5af7040d09ab6
Alex Chipre <alechipre@gmail.com>
2023-12-16 13:36:49 -0500

    Add Books Covers

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,5 +1,16 @@
+import Book from '../mockup/books.json'
+import './section.css'
 export default function Section () {
+  const informacion = Book
   return (
-    <div>Section</div>
+    <section className='grid-book'>
+      {informacion.map(info => (
+        <div className="card-book" key={info.Id}>
+          <img className="card-book__img" src={info.Imagen} alt={info.Titulos} />
+          <p className="card-book__title">{info.Titulos}</p>
+          <p className="card-book__autor">{info.Autores}</p>
+        </div>
+      ))}
+    </section>
   )
 }
