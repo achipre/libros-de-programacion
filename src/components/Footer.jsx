7391e42405c081835010e719984377ff75c1b1ce
Alex Chipre <alechipre@gmail.com>
2023-12-16 19:36:32 -0500

    Add Book and Section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,11 @@ import './footer.css'
 
 export default function Footer () {
   return (
-    <footer className='footer'>
-      <p>Escribeme <span>aqui </span> si quieres que agregue un libro</p>
+    <footer className="footer">
+      <p>
+        Escribeme <span>aqui </span> si quieres que agregue un libro
+      </p>
+
     </footer>
   )
 }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,10 +3,19 @@ import './section.css'
 export default function Section () {
   const informacion = Book
   return (
-    <section className='grid-book'>
+    <section className="grid-book">
       {informacion.map(info => (
         <div className="card-book" key={info.Id}>
-          <img className="card-book__img" src={info.Imagen} alt={info.Titulos} />
+          <div className="card-book__boximg">
+            <img className="card-book__img" src={info.Imagen} alt={info.Titulos} />
+            <div className="card-book__lang">
+              {info.Formato.map((lang, idx) => (
+                <a key={idx} className="lang__esp" href={lang.url} target='_blank' rel="noreferrer">
+                  {lang.formato}
+                </a>
+              ))}
+            </div>
+          </div>
           <p className="card-book__title">{info.Titulos}</p>
           <p className="card-book__autor">{info.Autores}</p>
         </div>
